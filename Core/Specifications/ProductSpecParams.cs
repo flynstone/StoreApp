e2ab4961c2d340773d72f5e59d32a3352669cc1f
Julien <flynstone@live.ca>
2022-07-14 13:27:28 -0400

    added bootstrap and initial app styling

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -19,10 +19,10 @@
 
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
-        public string Sort { get; set; }
+        public string? Sort { get; set; }
 
         private string _search;
-        public string Search
+        public string? Search
         {
             get => _search;
             set => _search = value.ToLower();
